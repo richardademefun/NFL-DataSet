e40c0611267000c9883d3b4f9245f2f72391a83e
richardademefun <58168348+richardademefun@users.noreply.github.com>
2019-11-27 15:27:40 +0000

    Add files via upload

--- python.py
+++ python.py
@@ -196,7 +196,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Function to check model accuracy
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+
+regressor = RandomForestRegressor(n_estimators = 50, random_state = 0, verbose=1)
 regressor.fit(X_train, y_train)
 
 y_pred = regressor.predict(X_test)
@@ -204,48 +205,13 @@ y_pred = regressor.predict(X_test)
 # Visualising the Random Forest Regression results (higher resolution)
 
 plt.scatter(y_test, y_pred, color = 'red')
-plt.plot(y_pred, regressor.predict(y_pred), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.title('predicted yards vs actual yards')
+plt.xlabel('Actual Yards')
+plt.ylabel('Predicted Yards')
 plt.show()
+from sklearn.metrics import mean_squared_error
+mean_squared_error(y_test, y_pred)
 
-
-thresholds = np.arange(70)
-
-def heavyside(actual):
-    return thresholds >= actual
-
-def is_cdf_valid(case):
-    if case[0] < 0 or case[0] > 1:
-        return False
-    for i in range(1, len(case)):
-        if case[i] > 1 or case[i] < case[i-1]:
-            return False
-    return True
-
-def calc_crps(predictions, actuals):
-    #some vector algebra for speed
-    obscdf = np.array([heavyside(i) for i in actuals])
-    crps = np.mean(np.mean((predictions - obscdf) ** 2))
-    return crps
-
-def CRPS(predictions, actuals):
-    
-    check = True
-    '''
-    for p in predictions :
-        if is_cdf_valid(p) == False : 
-            print 'something wrong with your prediction'
-            check = False
-            break
-    '''
-    if check == True : 
-         return calc_crps(predictions, actuals)
-         
-###example of usage
-predictions = np.ones((X_test.shape[0],70))
-print(CRPS(predictions, y_test))
 
 
 
