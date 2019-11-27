eb6aa10e085a718c5380c49c272031ce557cc0b1
richardademefun <58168348+richardademefun@users.noreply.github.com>
2019-11-27 12:39:45 +0000

    Add files via upload

--- python.py
+++ python.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+
+        
+        
 """
 Created on Tue Nov 19 18:29:12 2019
 
@@ -7,83 +9,104 @@ Created on Tue Nov 19 18:29:12 2019
 import numpy as np
 #import matplotlib.pyplot as plt
 import pandas as pd
+#
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
 
 # Importing the dataset
-dataset = pd.read_csv('train.csv')
+#dataset = pd.read_csv('/kaggle/input/nfl-big-data-bowl-2020/train.csv')
+dataset = pd.read_csv('train.csv',low_memory=False)
+
 #Find Out if there's any missing data
 
 #Doing somethimg about those pesky Nans
 #Orientation
 """ If Nan is present in Orientation i will replace it
- with the average orientation """
-Orient =  dataset['Orientation']
-MeanOrient = np.nanmean(Orient, axis=0)
-Orientation = Orient.fillna(MeanOrient)
+ with 0 category """
+dataset.loc[(dataset.Orientation >= 0) & (dataset.Orientation <= 45), 'Orientation'] = 1
+dataset.loc[(dataset.Orientation > 315) & (dataset.Orientation <= 360), 'Orientation'] = 1
+dataset.loc[(dataset.Orientation > 225) & (dataset.Orientation <= 315), 'Orientation'] = 4
+dataset.loc[(dataset.Orientation > 135) & (dataset.Orientation <= 225), 'Orientation'] = 3
+dataset.loc[(dataset.Orientation > 45) & (dataset.Orientation <= 135), 'Orientation'] = 2
+dataset['Orientation'].fillna(0, inplace=True) # if colum is nan give value of 0
+# Now orientation is in 5 categories
 
 #Dir
 """ If Nan is present in Dir i will replace it
- with the average Dir """
-Dir =  dataset['Dir']
-MeanDir = np.nanmean(Dir, axis=0)
-Direction = Dir.fillna(MeanDir)
+ with 0 """
+
+dataset.loc[(dataset.Dir >= 0) & (dataset.Dir <= 45), 'Dir'] = 1
+dataset.loc[(dataset.Dir > 315) & (dataset.Dir <= 360), 'Dir'] = 1
+dataset.loc[(dataset.Dir > 225) & (dataset.Dir <= 315), 'Dir'] = 4
+dataset.loc[(dataset.Dir > 135) & (dataset.Dir <= 225), 'Dir'] = 3
+dataset.loc[(dataset.Dir > 45) & (dataset.Dir <= 135), 'Dir'] = 2
+dataset['Dir'].fillna(0, inplace=True) # if colum is nan give value of 0
 
 #Humidity
-Humid =  dataset['Humidity']
-MeanHumid = np.nanmean(Humid, axis=0)
-Humidity = Humid.fillna(MeanHumid)
+dataset['Humidity']
+MeanHum = np.nanmean(dataset['Humidity'], axis=0)
+dataset['Humidity'].fillna(MeanHum, inplace=True) # if colum is nan give value of 0
 
-##WindSpeed
-#WindS =  dataset['WindSpeed']
-#MeanWindS = np.nanmean(WindS, axis=0)
-#WindSpeed = WindS.fillna(MeanWindS)
+
+# Weather
+dataset['GameWeather'].str.lower()
+dataset['GameWeather'].apply(lambda x: "indoor" if not pd.isna(x) and "indoor" in x else x)
+dataset['GameWeather'].apply(lambda x: x.replace('coudy', 'cloudy').replace('clouidy', 'cloudy').replace('party', 'partly') if not pd.isna(x) else x)
+dataset['GameWeather'].apply(lambda x: x.replace('clear and sunny', 'sunny and clear') if not pd.isna(x) else x)
+dataset['GameWeather'].apply(lambda x: x.replace('skies', '').replace("mostly", "").strip() if not pd.isna(x) else x)
 
 #Temperature
-Temp =  dataset['Temperature']
-MeanTemp = np.nanmean(Temp, axis=0)
-Temperature = Temp.fillna(MeanTemp)
+dataset['Temperature']
+MeanTemp = np.nanmean(dataset['Temperature'], axis=0)
+dataset['Temperature'].fillna(MeanTemp, inplace=True) # if colum is nan give value of 0
 
+#Defenders in the box
+dataset['DefendersInTheBox']
+MeanDef = np.nanmean(dataset['DefendersInTheBox'], axis=0)
+dataset['DefendersInTheBox'].fillna(MeanDef, inplace=True) # if colum is nan give value of 0
 
-#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
+#WindSpeed
+dataset['WindSpeed'] = pd.to_numeric(dataset.WindSpeed, errors='coerce')
+MeanWind = dataset['WindSpeed'].mean()
+dataset['WindSpeed'].fillna(MeanWind, inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-""" Replace categorical data with string blank"""
-newdf3 = dataset[['FieldPosition','OffenseFormation','StadiumType','Turf',
-                        'GameWeather','WindDirection']]
+#Wind Direction
+dataset['WindDirection'].fillna('unknown', inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-Nan_replacement = 'blank'
-newdf3.update(newdf3.fillna(Nan_replacement))
+#Game Weather
+dataset['GameWeather'].fillna('unknown', inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-#Combine the new dataframes
-CategoricalData_3 = pd.concat([newdf3, Direction,Orientation], axis=1)
+#Stadium type
+dataset['StadiumType'].fillna('unknown', inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-FullDataset = dataset.drop(['FieldPosition','OffenseFormation','StadiumType','Turf',
-                        'GameWeather','Temperature','Humidity',
-                        'WindSpeed','WindDirection','Dir','Orientation'], axis=1)
+#Field Position
+dataset['FieldPosition'].fillna('unknown', inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-NanFreeDataSet = pd.concat([newdf3, Direction,Orientation,FullDataset], axis=1)
+#Offense Formation
+dataset['OffenseFormation'].fillna('unknown', inplace=True) # if colum is nan give value of 0#Field Position, OffenseFormation,StadiumType, Turf, GameWeather, WindDirection
 
-X = NanFreeDataSet.drop(['GameId','PlayId','NflId','Yards'], axis=1)
-y = NanFreeDataSet['Yards']
+X = dataset.drop(['GameId','PlayId','NflId'], axis=1)
 
 #Separate the categorical data and numerical data
-CategoricalData = X[['Team','DisplayName','JerseyNumber','Season','YardLine',
-                         'Quarter','PossessionTeam','Down','Distance',
-                         'FieldPosition',
+CategoricalData = X[['Team','DisplayName','JerseyNumber','Season',
+                         'Quarter','PossessionTeam','Down','FieldPosition',
                          'NflIdRusher','OffenseFormation','OffensePersonnel',
-                         'PlayDirection','PlayerHeight','PlayerWeight',
-                         'PlayerBirthDate','PlayerCollegeName','Position',
-                         'HomeTeamAbbr','VisitorTeamAbbr','Week','Stadium',
+                         'PlayDirection','PlayerCollegeName','Position',
+                         'HomeTeamAbbr','VisitorTeamAbbr','Stadium','Down',
                          'Location','StadiumType','Turf','GameWeather',
-                         'WindDirection','Dir','Orientation','DefensePersonnel']]
+                         'WindDirection','Dir','Orientation',
+                         'DefensePersonnel']]
 
-NumericalData = X.drop(['Team','DisplayName','JerseyNumber','Season','YardLine',
-                         'Quarter','PossessionTeam','Down','Distance',
-                         'FieldPosition',
+NumericalData = X.drop(['Team','DisplayName','JerseyNumber','Season',
+                         'Quarter','PossessionTeam','Down','FieldPosition',
                          'NflIdRusher','OffenseFormation','OffensePersonnel',
-                         'PlayDirection','PlayerHeight','PlayerWeight',
-                         'PlayerBirthDate','PlayerCollegeName','Position',
-                         'HomeTeamAbbr','VisitorTeamAbbr','Week','Stadium',
+                         'PlayDirection','PlayerCollegeName','Position',
+                         'HomeTeamAbbr','VisitorTeamAbbr','Stadium','Down',
                          'Location','StadiumType','Turf','GameWeather',
-                         'WindDirection','Dir','Orientation','DefensePersonnel'], axis=1)
+                         'WindDirection','Dir','Orientation',
+                         'DefensePersonnel'], axis=1)
 
 #Calculating the Age of the players(closest age)
 ''' Changing date of birth to age '''
@@ -137,44 +160,92 @@ for i in range(0,len(Height)):
 #Adding and dropping the Altered variable back into the dataframe
 """ Numerical Data"""
 NumericalData_1 = NumericalData.drop(['TimeHandoff','TimeSnap',
-                    'GameClock'], axis=1)   
+                    'GameClock','PlayerHeight','PlayerBirthDate'], axis=1)   
 newdf1=pd.DataFrame(zip(throw_time,
-                        GameClock),columns=['ThrowTime','GameClock'])
+                        GameClock,H_inches,Age),columns=['ThrowTime',
+    'GameClock','PlayerHeight','PlayerBirthDate'])
 
 NumericalData_2 = pd.concat([NumericalData_1, newdf1], axis=1)
 
-""" Categorical Data"""
-CategoricalData_1 = CategoricalData.drop(['PlayerHeight',
-                                          'PlayerBirthDate'], axis=1)       
-newdf2=pd.DataFrame(zip(H_inches,
-                        Age),columns=['PlayerHeight','PlayerBirthDate'])
-CategoricalData_2 = pd.concat([CategoricalData_1, newdf2], axis=1)
-
+""" Encoding Categorical Data"""
 from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
 d = defaultdict(LabelEncoder)
 from sklearn.preprocessing import LabelEncoder
-fit = CategoricalData_2.apply(lambda x: d[x.name].fit_transform(x))
-
-newCatdf = (pd.get_dummies(CategoricalData_2, drop_first=True)).values
-newCatdf1 = newCatdf.iloc[:,0:100].values
+CategoricalData = (CategoricalData.apply(
+        lambda x: d[x.name].fit_transform(x)))
+CategoricalData = CategoricalData.to_numpy()
 #Scalling the numerical values
+
 from sklearn.preprocessing import MinMaxScaler
-
 scaler = MinMaxScaler()
-NumericalData_3 = scaler.fit_transform(NumericalData_2)
-
+y = NumericalData_2['Yards']
+NumericalData_22 = NumericalData_2.drop(['Yards'],axis = 1)
+NumericalData_3 = scaler.fit_transform(NumericalData_22)
 #combining the numerical and Categorical
-CatVar = newCatdf.to_numpy()
+FullData = np.concatenate((CategoricalData,NumericalData_3),axis=1)
+
+#rain test split
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+X = FullData
+y = y.to_numpy()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+
+# Function to check model accuracy
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+# Visualising the Random Forest Regression results (higher resolution)
+
+plt.scatter(y_test, y_pred, color = 'red')
+plt.plot(y_pred, regressor.predict(y_pred), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
 
+thresholds = np.arange(70)
 
+def heavyside(actual):
+    return thresholds >= actual
 
+def is_cdf_valid(case):
+    if case[0] < 0 or case[0] > 1:
+        return False
+    for i in range(1, len(case)):
+        if case[i] > 1 or case[i] < case[i-1]:
+            return False
+    return True
 
+def calc_crps(predictions, actuals):
+    #some vector algebra for speed
+    obscdf = np.array([heavyside(i) for i in actuals])
+    crps = np.mean(np.mean((predictions - obscdf) ** 2))
+    return crps
 
-
-
-
+def CRPS(predictions, actuals):
+    
+    check = True
+    '''
+    for p in predictions :
+        if is_cdf_valid(p) == False : 
+            print 'something wrong with your prediction'
+            check = False
+            break
+    '''
+    if check == True : 
+         return calc_crps(predictions, actuals)
+         
+###example of usage
+predictions = np.ones((X_test.shape[0],70))
+print(CRPS(predictions, y_test))
 
 
 
